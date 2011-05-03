875388d6a4764883d95a7519c24d51e223a02c42
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2011-05-03 10:27:31 -0400

    bundler y config

--- consumer.rb
+++ consumer.rb
@@ -1,29 +1,24 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
 
-require "rubygems"
-require "sinatra"
-require "oauth"
 require "oauth/consumer"
-#require 'grackle'
-require 'haml'
 
 enable :sessions
 
+CONFIG = YAML.load("config.yml")
+
 before do
   session[:oauth] ||= {}
-  
-  consumer_key = "9MPIIyzbSYRQuAvJZlRLESI8LrQHuMBd3cBRBQur"
-  consumer_secret = "NdikqpzEIK2CKhES9azsNZs1d2opQ9Xw9Xwv4mf5"
-  
+  consumer_key = CONFIG[:key]
+  consumer_secret = CONFIG[:secret]
   
   @consumer ||= OAuth::Consumer.new( consumer_key, consumer_secret , {
-      :site=>"http://localhost:3001"
-      #, 
+      :site=>CONFIG[:host]
       #:authorize_path => "/oauth/request_token", 
       #:access_token_path => "/oauth/access_token",
       #:authorize_path =>"/oauth/authorize"
   })
-
-  #@consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret, :site => "http://localhost:3000")
   
   if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
     @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
@@ -32,24 +27,10 @@ before do
   if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
     @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
   end
- 
-=begin  
-  if @access_token
-    @client = Grackle::Client.new(:auth => {
-      :type => :oauth,
-      :consumer_key => consumer_key,
-      :consumer_secret => consumer_secret,
-      :token => @access_token.token,
-      :token_secret => @access_token.secret
-    })
-  end
-=end
-
 end
 
 get "/" do
   if @access_token
-    #@statuses = @client.statuses.friends_timeline? :count => 100
     @data= @access_token.get('/causes.xml')
     @data = @data.body
     haml :index
@@ -65,11 +46,7 @@ get "/request" do
   redirect @request_token.authorize_url
 end
 
-#22reIHqHY18ipUHChUMpyWmnEiREVHyoWVwOxxCp&oauth_verifier=BnJyRsvzTC5yxCk9jJkW
-
 get "/auth" do
- # @request_token = @consumer.get_request_token(:oauth_callback => "http://artenlinea.cz/auth")
-  
   @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
   session[:oauth][:access_token] = @access_token.token
   session[:oauth][:access_token_secret] = @access_token.secret
